86e692318301cd9960187ee79f2e0e611014c8a4
Stathis Ioannidis <56734272+Stathis96@users.noreply.github.com>
2021-10-12 12:30:13 +0300

    Minorchanges
    Completed project, with CORS enabled to properly communicate with frontend and DB mysql

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // for allowing  access-control-allow-origin
   await app.listen(8000);
 }
 bootstrap();
